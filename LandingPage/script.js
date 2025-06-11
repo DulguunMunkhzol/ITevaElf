0c473adf772e62c46a9c6bd68cefd379c802f60f
DulguunMunkhzol <97315718+DulguunMunkhzol@users.noreply.github.com>
2025-06-11 12:41:46 -0400

    implemented addbook and displayBook functions

---
+++ LandingPage/script.js
@@ -0,0 +1,58 @@
+//mutable Variable
+let books = [];
+displayBooks();
+
+
+//  DOM (Document Object Model) manipulation
+//constant or final Variable
+const bookForm = document.getElementById('book-form')
+const bookContainer = document.getElementById('book-container')
+
+bookForm.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const publishDate = document.getElementById('publish-date').value;
+    const genre = document.getElementById('genre').value;
+    const price = document.getElementById('price').value;
+
+    //javascript object
+    const book = {
+        id: Date.now(),
+        title:title,
+        author:author,
+        publishDate:publishDate,
+        genre:genre,
+        price:price,
+        rating:0,
+        dateAdded: new Date()
+    };
+
+    books.push(book);
+
+    bookForm.reset();
+    alert(JSON.stringify(books))
+    displayBooks();
+})
+
+function displayBooks(){
+    bookContainer.innerHTML = "";
+
+    if(books.length===""){
+        bookContainer.innerHTML = '<p>No books yet, Add your First Book!</p>';
+        return;
+    }
+
+
+    books.forEach(book =>{
+        const bookRow = document.createElement('tr');
+        bookRow.innerHTML = `
+        <th>${book.title}</th>
+        <td>${book.author}</td>
+        <td>${book.publishDate}</td>
+        <td>${book.genre}</td>
+        `;
+        bookContainer.appendChild(bookRow);
+    })
+}
